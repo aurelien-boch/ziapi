de74b5ff5821dcc109574d62742a34ac9169ac5f
Aurélien BOCH <aurelien.boch@epitech.eu>
2022-01-19 15:59:26 +0100

    docs: fix comments
    fix comments in accordance to pull requests issues

--- include/IClient.hpp
+++ include/IClient.hpp
@@ -12,8 +12,8 @@ namespace api
 {
     /**
      * @interface IClient
-     * @brief Defines the basic behavior of a client. It can either be an asio encapsulation or your
-     * own socket abstraction.
+     * @brief Defines the basic behavior of a client. It can either be an asio abstraction or your
+     * own socket encapsulation.
      */
     class IClient
     {
@@ -25,14 +25,14 @@ namespace api
         #endif
 
             /**
-             * @brief Send items to the client through the socket. If the socket isn't able to write all
+             * @brief Sends items to the client through the socket. If the socket isn't able to write all
              * the bytes, it should throw an exception.
              * @param content[in] Refers to a vector that will contain all the elements to send.
              */
             virtual void send(std::vector<std::byte> const &content) const = 0;
 
             /**
-             * @brief Receives items from the client trough the socket. If there are no bytes to read
+             * @brief Receives items from the client through the socket. If there are no bytes to read
              * it should throw an exception.
              * @param bytesToRead[in] Refers to the number of bytes to read from the socket.
              * @return a vector containing the bytes that have been read.
@@ -43,7 +43,7 @@ namespace api
              * @brief Connects the socket to a given ip and port. If the socket is already connected (using
              * connect or as a client for example, it should throw an exception).
              * @param ip[in] Refers to the ip to connect to.
-             * @param port[in]Refers to the port to connect to.
+             * @param port[in] Refers to the port to connect to.
              */
             virtual void connect(std::uint32_t ip, std::uint16_t port) = 0;
 
