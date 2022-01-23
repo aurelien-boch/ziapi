23afd761f08f3b7c58c25907110d9b87ded75ad8
Aurélien BOCH <aurelien.boch@epitech.eu>
2022-01-23 12:10:42 +0100

    feat: Fix required changes in pull request

--- include/types/HttpReply.hpp
+++ include/types/HttpReply.hpp
@@ -8,22 +8,22 @@ namespace api
 {
     /**
      * @struct HTTPReply
-     * @brief This structure defines an example type for HTTP(S) responses.
+     * @brief Defines an example type for HTTP(S) responses.
      */
     struct HTTPReply
     {
         /**
-         * @brief This field refers to the response code.
+         * @brief Refers to the response code.
          */
         std::uint16_t code;
 
         /**
-         * @brief This string refers to the response body.
+         * @brief Refers to to the response body.
          */
         std::string body;
 
         /**
-         * @brief This map of strings refers to the response headers.
+         * @brief Refers to the response headers.
          * The key represents the header name and the value represents it's value.
          */
         std::unordered_map<std::string, std::string> headers;
--- include/types/HttpRequest.hpp
+++ include/types/HttpRequest.hpp
@@ -10,40 +10,40 @@ namespace api
 {
     /**
      * @struct HTTPRequest
-     * @brief This structure defines an example type for HTTP(S) reply.
+     * @brief Defines an example type for HTTP(S) reply.
      */
     struct HTTPRequest
     {
         /**
-         * @brief This string represents the URI that has been queried by user.
+         * @brief The URI that has been queried by user.
          */
         std::string uri;
 
         /**
-         * @brief This string refers to the method used by the client to query the server.
+         * @brief The method used by the client to query the server.
          * example: POST / GET / PUT ...
          */
         std::string method;
 
         /**
-         * @brief This string refers to the request body.
+         * @brief The request body.
          */
         std::string body;
 
         /**
-         * @brief This map refers to the query options provided by the client.
+         * @brief The query options provided by the client.
          * The key represents the value and the value represents the query value.
          */
         std::unordered_map<std::string, std::string> queryOptions;
 
         /**
-         * @brief This map refers to the query options provided by the client.
+         * @brief The query options provided by the client.
          * The key represents the value and the value represents the query value.
          */
         std::unordered_map<std::string, std::string> headers;
 
         /**
-         * @brief This map refers to the data that can be passed between middlewares through the request.
+         * @brief Data that can be passed between middlewares through the request.
          * The key represents the data name and the value is an any that can be used to store any type of data.
          */
         std::unordered_map<std::string, std::any> middlewareData;
